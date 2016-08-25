6d6342817e2e5b691a5bf2b723b66f21f65289dd
james tak <jtak93@gmail.com>
2016-08-25 13:31:34 -0700

    constructs all player piece objects into player arrays

--- js/javascript.js
+++ js/javascript.js
@@ -12,24 +12,46 @@ class CheckerPiece {
   }
 }
 
-var pieces = []; // Stores all piece objects
+var player1Pieces = []; // Stores all player 1 object
+var player2Pieces = []; // Stores player 2 objects
+
+// create piece objects and place into corresponding player array
 // create row 0 objects
 for (i = 0; i < 4; i++) {
-  var piece = new CheckerPiece("one", 1 + (2 * i), 0, false);
-  pieces.push(piece);
+  var newPiece = new CheckerPiece("one", 1 + (2 * i), 0, false);
+  player1Pieces.push(newPiece);
 };
 
 // create row 1 objects
 for (i = 0; i < 4; i++) {
-  var piece = new CheckerPiece("one", (2 * i), 1, false);
-  pieces.push(piece);
+  var newPiece = new CheckerPiece("one", (2 * i), 1, false);
+  player1Pieces.push(newPiece);
 };
 
 // create row 2 objects
 for (i = 0; i < 4; i++) {
-  var piece = new CheckerPiece("one", 1 + (2 * i), 2, false);
-  pieces.push(piece);
+  var newPiece = new CheckerPiece("one", 1 + (2 * i), 2, false);
+  player1Pieces.push(newPiece);
+};
+
+// create row 5 objects
+for (i = 0; i < 4; i++) {
+  var newPiece = new CheckerPiece("one", (2 * i), 5, true);
+  player2Pieces.push(newPiece);
+};
+
+// create row 6 objects
+for (i = 0; i < 4; i++) {
+  var newPiece = new CheckerPiece("one", 1 + (2 * i), 5, false);
+  player2Pieces.push(newPiece);
+};
+
+// create row 7 objects
+for (i = 0; i < 4; i++) {
+  var newPiece = new CheckerPiece("one", 1 + (2 * i), 5, false);
+  player2Pieces.push(newPiece);
 };
 
 
-
+console.log(player1Pieces);
+console.log(player2Pieces);
