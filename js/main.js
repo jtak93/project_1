ebde79f80f5b9209e9d92acef4052b975cc2fce9
james tak <jtak93@gmail.com>
2016-08-25 19:06:01 -0700

    makes change of game model to simpler single array

--- js/main.js
+++ js/main.js
@@ -2,15 +2,26 @@
 // constructor function to create initial objects
 // parameters of player number (0,1,2)
 class CheckerPiece {
-  constructor(player, xPos, yPos, validMove) {
+  constructor(player, pos, validMove) {
     this.player = player;
-    this.xPos = xPos;
-    this.yPos = yPos;
+    this.position = pos;
     this.king = false;
     this.validMove = validMove;
     this.jumped = false;
   }
 }
+
+// array to store board state for render function
+var board =
+[1, 1, 1, 1,
+ 1, 1, 1, 1,
+ 1, 1, 1, 1,
+ 0, 0, 0, 0,
+ 0, 0, 0, 0,
+ 2, 2, 2, 2,
+ 2, 2, 2, 2,
+ 2, 2, 2, 2]
+
 // stores number of player pieces;
 // decrement when piece is killed;
 var numPieces1 = 12;
@@ -19,42 +30,29 @@ var player1Pieces = []; // Stores all player 1 object
 var player2Pieces = []; // Stores player 2 objects
 
 // create piece objects and place into corresponding player array
-// create row 0 objects
-for (i = 0; i < 4; i++) {
-  var newPiece = new CheckerPiece("one", 1 + (2 * i), 0, false);
+// Create player 1 objects
+for (i = 0; i < 12; i++) {
+  var newPiece = new CheckerPiece("one", i, false);
   player1Pieces.push(newPiece);
-};
-
-// create row 1 objects
-for (i = 0; i < 4; i++) {
-  var newPiece = new CheckerPiece("one", (2 * i), 1, false);
-  player1Pieces.push(newPiece);
-};
-
-// create row 2 objects
-for (i = 0; i < 4; i++) {
-  var newPiece = new CheckerPiece("one", 1 + (2 * i), 2, false);
-  player1Pieces.push(newPiece);
-};
-
-// create row 5 objects
-for (i = 0; i < 4; i++) {
-  var newPiece = new CheckerPiece("one", (2 * i), 5, true);
+}
+// Create player 2 objects
+for (i = 20; i < 32; i++) {
+  var newPiece = new CheckerPiece("one", i, false);
   player2Pieces.push(newPiece);
-};
+}
 
-// create row 6 objects
-for (i = 0; i < 4; i++) {
-  var newPiece = new CheckerPiece("one", 1 + (2 * i), 5, false);
-  player2Pieces.push(newPiece);
-};
-
-// create row 7 objects
-for (i = 0; i < 4; i++) {
-  var newPiece = new CheckerPiece("one", 1 + (2 * i), 5, false);
-  player2Pieces.push(newPiece);
-};
-
+// Iterate through board array and append correct text or image to correct grid
+function render() {
+  for ( i = 0; i < board.length; i++) {
+    if (board[i] === 0) {
+    //   make space blank;
+  }else if (board[i] === 1){
+    // put in player 1 piece;
+  }else if (board[i] === 2){
+    // put player 2 piece;
+  }
+  }
+}
 
 
 console.log(player1Pieces);
