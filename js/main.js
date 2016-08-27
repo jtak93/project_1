64392d82ad5d55331f8ab97c2b1e435c62fe3d21
james tak <jtak93@gmail.com>
2016-08-27 15:06:19 -0700

    changed model to put piece objects into single array

--- js/main.js
+++ js/main.js
@@ -12,70 +12,96 @@ class CheckerPiece {
 }
 
 // array to store board state for render function
-var board =
-[1, 1, 1, 1,
- 1, 1, 1, 1,
- 1, 1, 1, 1,
- 0, 0, 0, 0,
- 0, 0, 0, 0,
- 2, 2, 2, 2,
- 2, 2, 2, 2,
- 2, 2, 2, 2]
- ;
+// var board =
+// [1, 1, 1, 1,
+//  1, 1, 1, 1,
+//  1, 1, 1, 1,
+//  0, 0, 0, 0,
+//  0, 0, 0, 0,
+//  2, 2, 2, 2,
+//  2, 2, 2, 2,
+//  2, 2, 2, 2]
+//  ;
 
 // stores number of player pieces;
 // decrement when piece is killed;
 var numPieces1 = 12;
 var numPieces2 = 12;
-var player1Pieces = []; // Stores all player 1 object
-var player2Pieces = []; // Stores player 2 objects
+var boardObjects = []; // Stores all board objects, see board array above
 
-// create piece objects and place into corresponding player array
 // Create player 1 objects
 for (i = 0; i < 12; i++) {
-  var newPiece = new CheckerPiece("one", i, false);
-  player1Pieces.push(newPiece);
+  var newPiece = new CheckerPiece('one', i, false);
+  boardObjects.push(newPiece);
 }
+
+// Create blank objects to fill empty spots
+
+for (i = 12; i < 20; i++) {
+  var newPiece = new CheckerPiece('', i, false);
+  boardObjects.push(newPiece);
+}
+
 // Create player 2 objects
 for (i = 20; i < 32; i++) {
-  var newPiece = new CheckerPiece("one", i, false);
-  player2Pieces.push(newPiece);
+  var newPiece = new CheckerPiece('two', i, false);
+  boardObjects.push(newPiece);
 }
+
 
 var $grid = $('.grid2');
 // Iterate through board array and append correct text or image to correct grid
 function render() {
-  for ( i = 0; i < board.length; i++) {
-    if (board[i] === 0) {
+  for ( i = 0; i < boardObjects.length; i++) {
+    if (boardObjects[i].player === '') {
       $grid.eq(i).html('');
-  }else if (board[i] === 1){
+  }else if (boardObjects[i].player === 'one'){
       $grid.eq(i).html('X');
-  }else if (board[i] === 2){
+  }else if (boardObjects[i].player === 'two'){
       $grid.eq(i).html('O');
   }
   }
 }
+function createPieces() {
+  // Create player 1 objects
+  for (i = 0; i < 12; i++) {
+    var newPiece = new CheckerPiece('one', i, false);
+    boardObjects.push(newPiece);
+  }
 
+  // Create blank objects to fill empty spots
+
+  for (i = 12; i < 20; i++) {
+    var newPiece = new CheckerPiece('', i, false);
+    boardObjects.push(newPiece);
+  }
+
+  // Create player 2 objects
+  for (i = 20; i < 32; i++) {
+    var newPiece = new CheckerPiece('two', i, false);
+    boardObjects.push(newPiece);
+  }
+}
 // reset board state and render
 function resetGame() {
-  board =
-  [1, 1, 1, 1,
-   1, 1, 1, 1,
-   1, 1, 1, 1,
-   0, 0, 0, 0,
-   0, 0, 0, 0,
-   2, 2, 2, 2,
-   2, 2, 2, 2,
-   2, 2, 2, 2];
-   render();
+  boardObjects = [];
+  createPieces();
+  render();
 }
+
+// check for winner
+function checkWin() {
+
+}
+
+// check for valid moves
+function checkValidMoves() {
+
+}
+
 
 // assign jquery pointer to reset button to variable
 // attach event listener to reset game button with resetGame function
 $resetBtn = $('#resetBtn');
 $resetBtn.on('click', resetGame);
 
-
-
-console.log(player1Pieces);
-console.log(player2Pieces);
