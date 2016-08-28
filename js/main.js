c2b868052049c902c5c9916248b068ebe5969ca4
james tak <jtak93@gmail.com>
2016-08-27 18:44:13 -0700

    refactored checkvalidmovesp1 into for loops

--- js/main.js
+++ js/main.js
@@ -85,135 +85,96 @@ function checkValidMovesP1() {
     if (playerTurn%2  === 0){
       // all player 1 pieces
       if (obj['player'] === 1) {
-        // check each position and check moves
-        if        (obj['position'] === 4) { //row 2
-          // check for open moves
-            if (!boardObjects[0]['full']) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 5) {
-            if (!boardObjects[0]['full'] || !boardObjects[1]['full']) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 6) {
-            if (!boardObjects[1]['full'] || !boardObjects[2]['full']) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 7) {
-            if (!boardObjects[2]['full'] || !boardObjects[3]['full']) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 8) { //row 3
-            if (!boardObjects[4]['full'] || !boardObjects[5]['full'] ||(boardObjects[5]['player'] === 2 && !boardObjects[1]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 9) {
-            if (!boardObjects[5]['full'] || (boardObjects[5]['player'] === 2 && !boardObjects[0]['full']) ||
-                !boardObjects[6]['full'] || (boardObjects[6]['player'] === 2 && !boardObjects[2]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 10) {
-            if (!boardObjects[6]['full'] || (boardObjects[6]['player'] === 2 && !boardObjects[1]['full']) ||
-                !boardObjects[7]['full'] || (boardObjects[7]['player'] === 2 && !boardObjects[3]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 11) {
-            if (!boardObjects[7]['full'] || (boardObjects[7]['player'] === 2 && !boardObjects[2]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 12) { // row 4
-            if (!boardObjects[8]['full'] || (boardObjects[8]['player'] === 2 && !boardObjects[5]['full']))
-              obj.validMove = true;
+        if (obj['position'] === 4) {
+          if (!boardObjects[0]['full']) {
+            obj.validMove = true;
           }
-        } else if (obj['position'] === 13) {
-            if (!boardObjects[8]['full'] || (boardObjects[8]['player'] === 2 && !boardObjects[4]['full']) ||
-                !boardObjects[9]['full'] || (boardObjects[9]['player'] === 2 && !boardObjects[6]['full'])) {
-              obj.validMove = true;
-          }
-        } else if (obj['position'] === 14) {
-            if (!boardObjects[9]['full'] || (boardObjects[9]['player'] === 2 && !boardObjects[5]['full']) ||
-                !boardObjects[10]['full'] || (boardObjects[10]['player'] === 2 && !boardObjects[7]['full'])) {
-              obj.validMove = true;
-          }
-        } else if (obj['position'] === 15) {
-            if (!boardObjects[10]['full'] || (boardObjects[10]['player'] === 2 && !boardObjects[6]['full']) ||
-                !boardObjects[11]['full']) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 16) { // row 5
-            if (!boardObjects[12]['full'] || !boardObjects[13]['full'] || (boardObjects[13]['player'] === 2 && !boardObjects[9]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 17) {
-            if (!boardObjects[13]['full'] || (boardObjects[13]['player'] === 2 && !boardObjects[8]['full']) ||
-                !boardObjects[14]['full'] || (boardObjects[14]['player'] === 2 && !boardObjects[10]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 18) {
-            if (!boardObjects[14]['full'] || (boardObjects[14]['player'] === 2 && !boardObjects[9]['full']) ||
-                !boardObjects[15]['full'] || (boardObjects[15]['player'] === 2 && !boardObjects[11]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 19) {
-            if (!boardObjects[15]['full'] || (boardObjects[15]['player'] === 2 && !boardObjects[10]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 20) { // row 6
-            if (!boardObjects[16]['full'] || (boardObjects[16]['player'] === 2 && !boardObjects[13]['full']))
-              obj.validMove = true;
-        } else if (obj['position'] === 21) {
-            if (!boardObjects[16]['full'] || (boardObjects[16]['player'] === 2 && !boardObjects[12]['full']) ||
-                !boardObjects[17]['full'] || (boardObjects[17]['player'] === 2 && !boardObjects[14]['full'])) {
-              obj.validMove = true;
-          }
-        } else if (obj['position'] === 22) {
-            if (!boardObjects[17]['full'] || (boardObjects[17]['player'] === 2 && !boardObjects[13]['full']) ||
-                !boardObjects[18]['full'] || (boardObjects[18]['player'] === 2 && !boardObjects[15]['full'])) {
-              obj.validMove = true;
-          }
-        } else if (obj['position'] === 23) {
-            if (!boardObjects[18]['full'] || (boardObjects[18]['player'] === 2 && !boardObjects[14]['full']) ||
-                !boardObjects[19]['full']) {
-              obj.validMove = true;
-          }
-        }  else if (obj['position'] === 24) { //row 7
-            if (!boardObjects[20]['full'] || !boardObjects[21]['full'] || (boardObjects[21]['player'] === 2 && !boardObjects[17]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 25) {
-            if (!boardObjects[21]['full'] || (boardObjects[21]['player'] === 2 && !boardObjects[16]['full']) ||
-                !boardObjects[22]['full'] || (boardObjects[22]['player'] === 2 && !boardObjects[18]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 26) {
-            if (!boardObjects[22]['full'] || (boardObjects[22]['player'] === 2 && !boardObjects[17]['full']) ||
-                !boardObjects[23]['full'] || (boardObjects[23]['player'] === 2 && !boardObjects[19]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 27) {
-            if (!boardObjects[23]['full'] || (boardObjects[23]['player'] === 2 && !boardObjects[18]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 28) { // row 8
-            if (!boardObjects[24]['full'] || (boardObjects[24]['player'] === 2 && !boardObjects[21]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 29) {
-            if (!boardObjects[24]['full'] || (boardObjects[24]['player'] === 2 && !boardObjects[20]['full']) ||
-                !boardObjects[25]['full'] || (boardObjects[25]['player'] === 2 && !boardObjects[22]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 30) {
-            if (!boardObjects[25]['full'] || (boardObjects[25]['player'] === 2 && !boardObjects[21]['full']) ||
-                !boardObjects[26]['full'] || (boardObjects[26]['player'] === 2 && !boardObjects[23]['full'])) {
-              obj.validMove = true;
-            }
-        } else if (obj['position'] === 31) {
-            if (!boardObjects[26]['full'] || (boardObjects[26]['player'] === 2 && !boardObjects[22]['full']) ||
-                !boardObjects[27]['full']) {
-              obj.validMove = true;
-            }
         } else {
+          // refactor checkmovep1
+          // 5,6,7
+          for (var i = 5; i < 8; i++) {
+            if (obj['position'] === i) {
+              if (!boardObjects[i-5]['full'] || !boardObjects[i-4]['full']) {
+                obj.validMove = true;
+              }
+            }
+          }
+          // pos 12,20,28
+          for (var i = 12; i < 29; i = i + 8) {
+            if (obj['position'] === i) {
+              if (!boardObjects[i-4]['full'] || (boardObjects[i-4]['player'] === 2 && !boardObjects[i-7]['full'])) {
+                obj.validMove = true;
+              }
+            }
+          }
+          // 13,21,29
+          for (var i = 13; i < 30; i = i + 8) {
+            if (obj['position'] === i) {
+              if (!boardObjects[i-5]['full'] || (boardObjects[i-5]['player'] === 2 && !boardObjects[i-9]['full']) ||
+                  !boardObjects[i-4]['full'] || (boardObjects[i-4]['player'] === 2 && !boardObjects[i-7]['full'])) {
+                obj.validMove = true;
+              }
+            }
+          }
+          // 14,22,30
+          for (var i = 14; i < 31; i = i + 8) {
+            if (obj['position'] === i) {
+              if (!boardObjects[i-5]['full'] || (boardObjects[i-5]['player'] === 2 && !boardObjects[i-9]['full']) ||
+                  !boardObjects[i-4]['full'] || (boardObjects[i-4]['player'] === 2 && !boardObjects[i-7]['full'])) {
+                obj.validMove = true;
+              }
+            }
+          }
+          // 15,23,31
+          for (var i = 15; i < 31; i = i + 8) {
+            if (obj['position'] === i) {
+              if (!boardObjects[i-5]['full'] || (boardObjects[i-5]['player'] === 2 && !boardObjects[i-9]['full']) ||
+                  !boardObjects[i-4]['full']) {
+                obj.validMove = true;
+              }
+            }
+          }
+
+          // 8,16,24
+          for (var i = 8; i < 25; i = i + 8) {
+              if (obj['position'] === i) {
+                if (!boardObjects[i-3]['full'] || (boardObjects[i-3]['player'] === 2 && !boardObjects[i-7]['full']) ||
+                    !boardObjects[i-4]['full']) {
+                  obj.validMove = true;
+                }
+              }
+          }
+
+          // 9,17,25
+          for (var i = 9; i < 26; i = i + 8) {
+            if (obj['position'] === i) {
+              if (!boardObjects[i-4]['full'] || (boardObjects[i-4]['player'] === 2 && !boardObjects[i-9]['full']) ||
+                  !boardObjects[i-3]['full'] || (boardObjects[i-3]['player'] === 2 && !boardObjects[i-7]['full'])) {
+                obj.validMove = true;
+              }
+            }
+          }
+
+          // 10,18,26
+          for (var i = 10; i < 27; i = i + 8) {
+            if (obj['position'] === i) {
+              if (!boardObjects[i-4]['full'] || (boardObjects[i-4]['player'] === 2 && !boardObjects[i-9]['full']) ||
+                  !boardObjects[i-3]['full'] || (boardObjects[i-3]['player'] === 2 && !boardObjects[i-7]['full'])) {
+                obj.validMove = true;
+              }
+            }
+          }
+
+          // 11,29,27
+          for (var i = 11; i < 28; i = i + 8) {
+            if (obj['position'] === i) {
+              if (!boardObjects[i-4]['full'] || (boardObjects[i-4]['player'] === 2 && !boardObjects[i-9]['full'])) {
+                obj.validMove = true;
+              }
+            }
+          }
+        }
+      } else {
             obj.validMove = false;
         }
       }
